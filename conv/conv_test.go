f882873d15be06462ccf28d608ca4ceac2ecf23d
NoraGith <noraolo@uia.no>
2023-02-20 11:53:08 +0100

    ryddet i koden

--- conv/conv_test.go
+++ conv/conv_test.go
@@ -5,15 +5,6 @@ import (
 	"testing"
 )
 
-/*
-*
-
-	Mal for testfunksjoner
-	Du skal skrive alle funksjonene basert på denne malen
-	For alle konverteringsfunksjonene (tilsammen 6)
-	kan du bruke malen som den er (du må selvsagt endre
-	funksjonsnavn og testverdier)
-*/
 func TestFahrenheitToCelsius(t *testing.T) {
 	type test struct {
 		input float64
@@ -32,8 +23,6 @@ func TestFahrenheitToCelsius(t *testing.T) {
 	}
 }
 
-// De andre testfunksjonene implementeres her
-// ...
 func TestCelsiusToFahrenheit(t *testing.T) {
 	type test struct {
 		input float64
