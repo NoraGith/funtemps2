f66bf348b81d88fe08be990f8b2798a24859754b
Martin Fidjeland <fidjeland@MacBook.local>
2023-02-20 10:17:49 +0100

    oppdatert funksjoner

--- main.go
+++ main.go
@@ -10,39 +10,42 @@ var fahrenheit float64
 var celsius float64
 var kelvin float64
 var out string
+var t string
 
 func init() {
 	flag.Float64Var(&fahrenheit, "F", 0.0, "temperatur i grader fahrenheit")
-	flag.Float64Var(&celsius, "F", 0.0, "temperatur i grader celsius")
-	flag.Float64Var(&kelvin, "F", 0.0, "temperatur i grader kelvin")
+	flag.Float64Var(&celsius, "C", 0.0, "temperatur i grader celsius")
+	flag.Float64Var(&kelvin, "K", 0.0, "temperatur i grader kelvin")
 	flag.StringVar(&out, "out", "C", "beregne temperatur i C - celsius, F - farhenheit, K- Kelvin")
+	flag.StringVar(&t, "t", "0.0", "temperaturskala, C, K eller F for funfacts")
 }
 
 func main() {
+
 	flag.Parse()
 
-	if out == "C" && isFlagPassed("F") {
-		fmt.Printf("%v°F er = %.2f°C", fahrenheit, conv.FahrenheitToCelsius(fahrenheit))
+	if isFlagPassed("out") {
+		if out == "C" && isFlagPassed("F") {
+			fmt.Println(conv.FahrenheitToCelsius(fahrenheit))
+		} else if out == "F" && isFlagPassed("C") {
+			fmt.Println(conv.CelsiusToFahrenheit(celsius))
+		}
 	}
 
-	if out == "K" && isFlagPassed("F") {
-		fmt.Printf("%v°F er = %.2f°K", fahrenheit, conv.FahrenheitToKelvin(fahrenheit))
+	if isFlagPassed("out") {
+		if out == "K" && isFlagPassed("C") {
+			fmt.Println(conv.KelvinToCelsius(celsius))
+		} else if out == "C" && isFlagPassed("K") {
+			fmt.Println(conv.CelsiusToKelvin(kelvin))
+		}
 	}
 
-	if out == "K" && isFlagPassed("C") {
-		fmt.Printf("%v°F er = %.2f°C", celsius, conv.CelsiusToKelvin(celsius))
-	}
-
-	if out == "F" && isFlagPassed("C") {
-		fmt.Printf("%v°F er = %.2f°C", celsius, conv.CelsiusToFahrenheit(celsius))
-	}
-
-	if out == "C" && isFlagPassed("K") {
-		fmt.Printf("%v°K er = %.2f°C", kelvin, conv.KelvinToCelsius(kelvin))
-	}
-
-	if out == "F" && isFlagPassed("K") {
-		fmt.Printf("%v°K er = %.2f°F", kelvin, conv.KelvinToFahrenheit(kelvin))
+	if isFlagPassed("out") {
+		if out == "K" && isFlagPassed("F") {
+			fmt.Println(conv.FahrenheitToKelvin(kelvin))
+		} else if out == "F" && isFlagPassed("K") {
+			fmt.Println(conv.KelvinToFahrenheit(fahrenheit))
+		}
 	}
 }
 
