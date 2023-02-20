f0af8aad59a5aefaf51c4a94f60dda2b1e5baf2d
Martin Fidjeland <fidjeland@MacBook.local>
2023-02-20 10:29:16 +0100

    La til main.go

--- main.go
+++ main.go
@@ -10,20 +10,16 @@ var fahrenheit float64
 var celsius float64
 var kelvin float64
 var out string
-var t string
 
 func init() {
 	flag.Float64Var(&fahrenheit, "F", 0.0, "temperatur i grader fahrenheit")
 	flag.Float64Var(&celsius, "C", 0.0, "temperatur i grader celsius")
 	flag.Float64Var(&kelvin, "K", 0.0, "temperatur i grader kelvin")
 	flag.StringVar(&out, "out", "C", "beregne temperatur i C - celsius, F - farhenheit, K- Kelvin")
-	flag.StringVar(&t, "t", "0.0", "temperaturskala, C, K eller F for funfacts")
 }
 
 func main() {
-
 	flag.Parse()
-
 	if isFlagPassed("out") {
 		if out == "C" && isFlagPassed("F") {
 			fmt.Println(conv.FahrenheitToCelsius(fahrenheit))
